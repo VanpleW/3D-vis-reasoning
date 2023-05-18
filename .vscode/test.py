e0a151cc02f8eda05cb64b8c6e8e07415219f34d
VanpleW <vanplewang@g.ucla.edu>
2023-05-18 12:14:29 -0700

    some basics on stage 1 models

--- .vscode/test.py
+++ .vscode/test.py
@@ -1,3 +1,30 @@
 '''
-    Unit test for the project
+    test different stages of the project
 '''
+import numpy as np
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.metrics import structural_similarity as ssim
+from PIL import Image
+import pandas as pd
+
+from utils import load_dummy, load_images, solver, psd, psd_noise
+
+
+def test_dummy_stage1(noise_type: str, method:str) -> list:
+    """ 
+        dummy test for stage 1 noise images with data provided by the course,
+        input the noise type and return a list of psnr and ssim
+    """
+    stats = pd.DataFrame(columns=['noise_type', 'method', 'psnr', 'ssim'])
+    # choose denoise method
+    sol = solver(method)
+
+    for img, noise_img in load_dummy(noise_type):
+        img = np.array(Image.open(img))
+        noise_img = np.array(Image.open(noise_img))
+        # denoise with the solver
+        dn_img = sol(noise_img, noise_type)
+        psnr_val = psnr(img, noise_img)
+        ssim_val = ssim(img, noise_img, multichannel=True)
+        # load PSNR and SSIM into a list according to the noise type and method
+        stats.loc[len(stats)] = [noise_type, method, psnr_val, ssim_val]
--- utils.py
+++ utils.py
@@ -1,3 +1,34 @@
+import numpy as np
+import os
+import cv2
+from natsort import natsorted
+from glob import glob
+
+from stage1.denoise import SpatialFilter, FrequencyFilter
+
+# load dummy images from a folder
+def load_dummy(noise_type: str) -> list:
+    path = 'stage1/' + noise_type + '/*'
+    noise_files = natsorted(glob(path))
+    clean_files = natsorted(glob('stage1/input_imgs/*'))
+    return zip(noise_files, clean_files)
+
+# load images from uploaded test_folder
+def load_images(path: str) -> list:
+    return None
+
+# select denoise method
+def solver(method: str):
+    solverDict = {
+        'median': SpatialFilter.median_filter,
+        'bilateral': SpatialFilter.bilateral_filter,
+        'wiener': SpatialFilter.wiener_filter,
+        'wavelet': FrequencyFilter.wavelet_filter
+        }
+    return solverDict[method]
+
+
+
 # calculate the power spectrum density of an image
 def psd(img: np.array) -> np.array:
 
@@ -8,6 +39,7 @@ def psd(img: np.array) -> np.array:
     # calculate the power spectrum density
     img_psd = np.abs(img_fft_shift) ** 2
     return img_psd
+
 
 # calculate the power spectrum density of noise inside a image
 def psd_noise(img: np.array, noise_type: str) -> np.array:
